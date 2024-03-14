fb9932c70822e5d84ced71bc600f80e657b21c3d
Amel Varghese <107236437+RlNZLER@users.noreply.github.com>
2024-03-14 21:15:50 +0000

    Add gpu_test file to check if tensor is able to detect GPU.

---
+++ src/gpu_test.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+# Print out the available physical devices
+print("Available Physical Devices:")
+physical_devices = tf.config.list_physical_devices()
+for device in physical_devices:
+    print(device)
+
+# Print out the available GPUs
+print("\nAvailable GPUs:")
+gpus = tf.config.list_physical_devices('GPU')
+for gpu in gpus:
+    print(gpu)
+
+# Print out information about GPU usage by TensorFlow
+print("\nTensorFlow GPU Usage:")
+print(tf.config.experimental.list_logical_devices('GPU'))
